c3bcd96e4b2cdb8e7b4e71c99259c4c78b768c8e
Warwick Stone <uozu.aho@gmail.com>
2019-10-05 14:03:55 +1000

    init maven app

---
+++ src/main/java/aho/uozu/YatzyConsoleApp.java
@@ -0,0 +1,9 @@
+package aho.uozu;
+
+public class YatzyConsoleApp
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/aho/uozu/YatzyConsoleAppTest.java
@@ -0,0 +1,14 @@
+package aho.uozu;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class YatzyConsoleAppTest
+{
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
