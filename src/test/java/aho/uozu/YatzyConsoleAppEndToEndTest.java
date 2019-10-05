e13b9325d0521e0c123aa1372b8b1f445cfc7e88
Warwick Stone <uozu.aho@gmail.com>
2019-10-05 14:13:04 +1000

    create first e2e test, with minimal implementation to compile

---
+++ src/test/java/aho/uozu/YatzyConsoleAppEndToEndTest.java
@@ -0,0 +1,17 @@
+package aho.uozu;
+
+import org.junit.Test;
+
+public class YatzyConsoleAppEndToEndTest
+{
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        var game = new YatzyConsoleAppRunner();
+        game.start();
+        game.displaysRoll();
+        game.playerChoosesCategory(ScoreCategory.CHANCE);
+        game.displaysScore();
+        game.isFinished();
+    }
+}
---
+++ src/test/java/aho/uozu/YatzyConsoleAppRunner.java
@@ -0,0 +1,23 @@
+package aho.uozu;
+
+public class YatzyConsoleAppRunner {
+    public void start() {
+
+    }
+
+    public void displaysRoll() {
+
+    }
+
+    public void playerChoosesCategory(ScoreCategory category) {
+
+    }
+
+    public void displaysScore() {
+
+    }
+
+    public void isFinished() {
+
+    }
+}
