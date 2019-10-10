048e29b0d636ff192ec648855cdd06932f2fb696
Warwick Stone <uozu.aho@gmail.com>
2019-10-10 19:36:46 +1100

    add Roll.toString, fix one test

--- src/main/java/aho/uozu/Roll.java
+++ src/main/java/aho/uozu/Roll.java
@@ -1,9 +1,19 @@
 package aho.uozu;
 
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
 public class Roll {
     private final int[] _diceValues;
 
     public Roll(int[] diceValues) {
         _diceValues = diceValues;
+    }
+
+    @Override
+    public String toString() {
+        return Arrays.stream(_diceValues)
+                .mapToObj(String::valueOf)
+                .collect(Collectors.joining(", "));
     }
 }
