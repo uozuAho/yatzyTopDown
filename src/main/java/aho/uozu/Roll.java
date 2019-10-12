0e33be38606ceb3c57f8410886244650832d72fb
Warwick Stone <uozu.aho@gmail.com>
2019-10-12 22:00:55 +1100

    better score calculator interface

--- src/main/java/aho/uozu/Roll.java
+++ src/main/java/aho/uozu/Roll.java
@@ -7,6 +7,9 @@ public class Roll {
     private final int[] _diceValues;
 
     public Roll(int[] diceValues) {
+        if (diceValues == null) throw new NullPointerException();
+        if (diceValues.length != 5) throw new IllegalArgumentException("must have 5 dice values");
+
         _diceValues = diceValues;
     }
 
