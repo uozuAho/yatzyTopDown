2daf19ef1a564d58985c9ba6142d29a0badd2198
Warwick Stone <uozu.aho@gmail.com>
2019-10-13 11:02:41 +1100

    fix yatzy score, all tests pass

--- src/main/java/aho/uozu/ScoreCalculator.java
+++ src/main/java/aho/uozu/ScoreCalculator.java
@@ -7,8 +7,17 @@ public class ScoreCalculator {
         if (category == ScoreCategory.CHANCE)
             return Arrays.stream(roll.getValues()).sum();
         if (category == ScoreCategory.YATZY)
-            return 50;
+            return calculateYatzyScore(roll);
 
         throw new IllegalStateException("Unknown category: " + category);
+    }
+
+    private int calculateYatzyScore(Roll roll) {
+        var values = roll.getValues();
+        var firstValue = values[0];
+        if (Arrays.stream(values).allMatch(value -> value == firstValue)) {
+            return 50;
+        }
+        return 0;
     }
 }
