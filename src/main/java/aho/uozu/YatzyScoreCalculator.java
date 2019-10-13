acb33d8f87587755e8d3751aae2e4f355b75b4b3
Warwick Stone <uozu.aho@gmail.com>
2019-10-13 11:16:14 +1100

    fix yatzy score

--- src/main/java/aho/uozu/YatzyScoreCalculator.java
+++ src/main/java/aho/uozu/YatzyScoreCalculator.java
@@ -1,7 +1,14 @@
 package aho.uozu;
 
+import java.util.Arrays;
+
 public class YatzyScoreCalculator {
     public int calculateScore(Roll roll) {
+        var values = roll.getValues();
+        var firstValue = values[0];
+        if (Arrays.stream(values).allMatch(value -> value == firstValue)) {
+            return 50;
+        }
         return 0;
     }
 }
