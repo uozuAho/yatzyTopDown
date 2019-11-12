d445d37c207cdd804c3be95b6a2ca874dda1062c
Warwick Stone <uozu.aho@gmail.com>
2019-11-12 20:28:45 +1100

    minor e2e test cleanup, fix test of displayed category order

--- src/test/java/aho/uozu/test/YatzyConsoleAppRunner.java
+++ src/test/java/aho/uozu/test/YatzyConsoleAppRunner.java
@@ -2,6 +2,8 @@ package aho.uozu.test;
 
 import aho.uozu.*;
 
+import java.util.ArrayList;
+
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.*;
@@ -37,11 +39,16 @@ public class YatzyConsoleAppRunner {
         assertTrue(_game.isFinished());
     }
 
-    public void displayedAvailableCategories(ScoreCategoryWithScore[] catScores) {
+    public void displayedAvailableCategoriesInAnyOrder(ScoreCategoryWithScore[] catScores) {
         assertThat(_consoleOutput.readNextLine(), is(equalTo("available categories:")));
+        var outputLines = new ArrayList<String>();
+        for (int i = 0; i < catScores.length; i++) {
+            outputLines.add(_consoleOutput.readNextLine());
+        }
         for (var catScore : catScores) {
             var expectedDisplay = String.format("    %s: %d points", catScore.category, catScore.score);
-            assertThat(_consoleOutput.readNextLine(), is(equalTo(expectedDisplay)));
+            var matchFound = outputLines.stream().anyMatch(outputLine -> expectedDisplay.equals(outputLine));
+            assertThat("Found expected category and score in output: " + expectedDisplay, matchFound, is(true));
         }
     }
 }
--- src/test/java/aho/uozu/test/tests/YatzyConsoleAppEndToEndTest.java
+++ src/test/java/aho/uozu/test/tests/YatzyConsoleAppEndToEndTest.java
@@ -14,8 +14,10 @@ public class YatzyConsoleAppEndToEndTest
     {
         final var constantRoll = new DiceRoll(new int[] {1, 1, 1, 1, 1});
         var diceRoller = new ConstantDiceRoller(constantRoll);
-        var player = new YatzyPlayerMock();
+        final var chanceScore = 5;
+        final var yatzyScore = 50;
 
+        var player = new YatzyPlayerMock();
         player.addNextInput(ScoreCategory.CHANCE);
         player.addNextInput(ScoreCategory.YATZY);
 
@@ -24,20 +26,20 @@ public class YatzyConsoleAppEndToEndTest
 
         // turn 1
         game.displayedRoll(constantRoll);
-        game.displayedAvailableCategories(new ScoreCategoryWithScore[] {
-                new ScoreCategoryWithScore(ScoreCategory.CHANCE, 5),
-                new ScoreCategoryWithScore(ScoreCategory.YATZY, 50)
+        game.displayedAvailableCategoriesInAnyOrder(new ScoreCategoryWithScore[] {
+                new ScoreCategoryWithScore(ScoreCategory.CHANCE, chanceScore),
+                new ScoreCategoryWithScore(ScoreCategory.YATZY, yatzyScore)
         });
         game.promptedUserForCategory();
-        game.displayedScore(5);
+        game.displayedScore(chanceScore);
 
         // turn 2
         game.displayedRoll(constantRoll);
-        game.displayedAvailableCategories(new ScoreCategoryWithScore[] {
-                new ScoreCategoryWithScore(ScoreCategory.YATZY, 50)
+        game.displayedAvailableCategoriesInAnyOrder(new ScoreCategoryWithScore[] {
+                new ScoreCategoryWithScore(ScoreCategory.YATZY, yatzyScore)
         });
         game.promptedUserForCategory();
-        game.displayedScore(50);
+        game.displayedScore(yatzyScore);
 
         game.gameIsOver();
     }
