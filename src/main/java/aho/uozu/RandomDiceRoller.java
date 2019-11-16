2353c1d33007721001f0363f41534f72b59cb4ef
Warwick Stone <uozu.aho@gmail.com>
2019-11-16 13:51:53 +1100

    RandomDiceRoller is now random

--- src/main/java/aho/uozu/RandomDiceRoller.java
+++ src/main/java/aho/uozu/RandomDiceRoller.java
@@ -1,9 +1,13 @@
 package aho.uozu;
 
+import java.util.Random;
+
 public class RandomDiceRoller implements DiceRoller {
+
+    private Random _rng = new Random();
+
     @Override
     public DiceRoll nextRoll() {
-        // todo: randomise!
-        return new DiceRoll(new int[] {1, 1, 1, 1, 1});
+        return new DiceRoll(_rng.ints(5, 1, 7).toArray());
     }
 }
